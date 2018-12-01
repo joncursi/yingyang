59376df26f0a66fcb1456c22068eb34f085edda0
Jon Cursi <joncursi@gmail.com>
2018-12-01 18:39:37 -0500

    test: Gets rid of jest

--- .babelrc.js
+++ .babelrc.js
@@ -7,28 +7,5 @@
 'use strict';
 
 module.exports = {
-  env: {
-    development: {
-      presets: ['next/babel', '@babel/preset-flow'],
-    },
-    production: {
-      presets: ['next/babel', '@babel/preset-flow'],
-    },
-    staging: {
-      presets: ['next/babel', '@babel/preset-flow'],
-    },
-    test: {
-      presets: [
-        [
-          'next/babel',
-          {
-            'preset-env': {
-              modules: 'commonjs',
-            },
-          },
-        ],
-        '@babel/preset-flow',
-      ],
-    },
-  },
+  presets: ['next/babel', '@babel/preset-flow'],
 };
