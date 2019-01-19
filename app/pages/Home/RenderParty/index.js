9f7adb1ff96db5509b6bff2419970e91b023f85c
Jon Cursi <joncursi@gmail.com>
2019-01-19 15:32:44 -0500

    feat: Separate the girls from the guys

--- app/pages/Home/RenderParty/index.js
+++ app/pages/Home/RenderParty/index.js
@@ -7,81 +7,34 @@ import * as React from 'react';
 
 import styles from './styles';
 
-const RenderParty = (): React.Node => {
-  const people = [
-    {
-      imageSrc: '/static/img/headshots/kayla.jpg',
-      name: 'Kayla Pillar',
-      title: 'Matron of Honor',
-    },
-    {
-      imageSrc: '/static/img/headshots/nikki.jpg',
-      name: 'Nicole Danley',
-    },
-    {
-      imageSrc: '/static/img/headshots/kris.jpg',
-      name: 'Kristina Wieckowski',
-    },
-    {
-      imageSrc: '/static/img/headshots/jess.jpg',
-      name: 'Jessica Dering',
-    },
-    {
-      imageSrc: '/static/img/headshots/emily.jpg',
-      name: 'Emily Housel',
-    },
-    {
-      imageSrc: '/static/img/headshots/noelle.jpg',
-      name: 'Noelle Vochansky',
-    },
-    {
-      imageSrc: '/static/img/headshots/lee.jpg',
-      name: 'Leonard "Lee" Cursi',
-      title: 'Best Man',
-    },
-    {
-      imageSrc: '/static/img/headshots/dave.jpg',
-      name: 'David Danley',
-    },
-    {
-      imageSrc: '/static/img/headshots/brian.jpg',
-      name: 'Brian Wieckowski',
-    },
-    {
-      imageSrc: '/static/img/headshots/kevin.jpg',
-      name: 'Kevin Lombarski',
-    },
-    {
-      imageSrc: '/static/img/headshots/frank.jpg',
-      name: 'Frank Pillar',
-    },
-    {
-      imageSrc: '/static/img/headshots/chris.jpg',
-      name: 'Christopher DeOliveira',
-    },
-  ];
+type PropsFlowType = {
+  people: Array<{
+    imageSrc: string,
+    name: string,
+    title: string,
+  }>,
+};
 
-  return (
-    <React.Fragment>
-      <style jsx>{styles}</style>
+const RenderParty = ({ people }: PropsFlowType): React.Node => (
+  <React.Fragment>
+    <style jsx>{styles}</style>
 
-      {people.map(
-        (person): React.Node => (
-          <div className="person" key={person.name}>
-            <img alt={person.name} src={person.imageSrc} />
+    {people.map(
+      (person): React.Node => (
+        <div className="person" key={person.name}>
+          <img alt={person.name} src={person.imageSrc} />
 
-            <div className="details">
-              <div>
-                <h2>{person.name}</h2>
+          <div className="details">
+            <div>
+              <h2>{person.name}</h2>
 
-                {person.title && <div className="title">{person.title}</div>}
-              </div>
+              {person.title && <div className="title">{person.title}</div>}
             </div>
           </div>
-        ),
-      )}
-    </React.Fragment>
-  );
-};
+        </div>
+      ),
+    )}
+  </React.Fragment>
+);
 
 export default RenderParty;
--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -78,9 +78,69 @@ const Home = (): React.Node => {
 
           <RenderColors />
 
-          <TextDivider text="Bridal Party" />
+          <TextDivider text="The Girls" />
 
-          <RenderParty />
+          <RenderParty
+            people={[
+              {
+                imageSrc: '/static/img/headshots/kayla.jpg',
+                name: 'Kayla Pillar',
+                title: 'Matron of Honor',
+              },
+              {
+                imageSrc: '/static/img/headshots/nikki.jpg',
+                name: 'Nicole Danley',
+              },
+              {
+                imageSrc: '/static/img/headshots/kris.jpg',
+                name: 'Kristina Wieckowski',
+              },
+              {
+                imageSrc: '/static/img/headshots/jess.jpg',
+                name: 'Jessica Dering',
+              },
+              {
+                imageSrc: '/static/img/headshots/emily.jpg',
+                name: 'Emily Housel',
+              },
+              {
+                imageSrc: '/static/img/headshots/noelle.jpg',
+                name: 'Noelle Vochansky',
+              },
+            ]}
+          />
+
+          <TextDivider text="The Guys" />
+
+          <RenderParty
+            people={[
+              {
+                imageSrc: '/static/img/headshots/lee.jpg',
+                name: 'Leonard "Lee" Cursi',
+                title: 'Best Man',
+              },
+              {
+                imageSrc: '/static/img/headshots/dave.jpg',
+                name: 'David Danley',
+              },
+              {
+                imageSrc: '/static/img/headshots/brian.jpg',
+                name: 'Brian Wieckowski',
+              },
+              {
+                imageSrc: '/static/img/headshots/kevin.jpg',
+                name: 'Kevin Lombarski',
+              },
+              {
+                imageSrc: '/static/img/headshots/frank.jpg',
+                name: 'Frank Pillar',
+              },
+              {
+                imageSrc: '/static/img/headshots/chris.jpg',
+                name: 'Christopher DeOliveira',
+              },
+            ]}
+          />
         </div>
       </PageLayout>
     </React.Fragment>
