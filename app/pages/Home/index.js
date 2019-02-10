2eb2621379173901af1dfbc1d8cd5785693bae72
Jon Cursi <joncursi@gmail.com>
2019-02-09 22:02:13 -0500

    chore: Package upgrades; temporarily remove colors

--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -9,7 +9,7 @@ import moment from 'moment';
 import PageLayout from '../../layouts/PageLayout';
 import TextDivider from '../../components/TextDivider';
 
-import RenderColors from './RenderColors';
+// import RenderColors from './RenderColors';
 import RenderParty from './RenderParty';
 import styles from './styles';
 
@@ -73,9 +73,14 @@ const Home = (): React.Node => {
             </a>
           </h3>
 
+          {/*
+
+
           <TextDivider text="Our Colors" />
 
           <RenderColors />
+          
+          */}
 
           <TextDivider text="The Girls" />
 
